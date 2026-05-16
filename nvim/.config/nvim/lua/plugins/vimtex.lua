f0c0c5f2b00bc102a02986b219724a5c07657995
waxkb <maxwellr2028@gmail.com>
2026-05-16 11:45:17 -0700

    e

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -1,25 +1,15 @@
 return {
-  {
-    "lervag/vimtex",
-    lazy = false, -- VimTeX recommends not lazy-loading for full functionality
-    init = function()
-      -- Vim-plug: let g:tex_flavor='latex'
-      vim.g.tex_flavor = "latex"
-
-      -- Vim-plug: let g:vimtex_view_method='zathura'
-      vim.g.vimtex_view_method = "zathura"
-
-      -- Vim-plug: let g:vimtex_quickfix_mode=0
-      vim.g.vimtex_quickfix_mode = 0
-
-      -- Vim-plug: let g:tex_conceal='abdmg'
-      vim.g.tex_conceal = "abdmg"
-
-    end,
-    config = function()
-      -- Vim-plug: set conceallevel=1
-      -- We set this in 'config' or 'init' to ensure it applies to TeX files
-      vim.opt.conceallevel = 1
-    end,
-  },
+    {
+        "lervag/vimtex",
+        lazy = false,
+        init = function()
+            vim.g.tex_flavor = "latex"
+            vim.g.vimtex_view_method = "zathura"
+            vim.g.vimtex_quickfix_mode = 0
+            vim.g.tex_conceal = "abdmg"
+        end,
+        config = function()
+            vim.opt.conceallevel = 1
+        end,
+    },
 }
