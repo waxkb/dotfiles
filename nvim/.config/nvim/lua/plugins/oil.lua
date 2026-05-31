d542de3c51cd5ec34d0eef94f04aef4d67064ed0
waxkb <maxwellr2028@gmail.com>
2026-05-31 11:36:37 -0700

    e

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,7 @@
 return {
   {
-    'stevearc/oil.nvim',
+    "stevearc/oil.nvim",
+    event = "BufReadPre",
     ---@module 'oil'
     ---@type oil.SetupOpts
     opts = {},
@@ -8,6 +9,6 @@ return {
     dependencies = { { "nvim-mini/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-    lazy = false,
-  }
+    -- lazy = false,
+  },
 }
