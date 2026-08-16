d65b6a7e652a91f46e2f2db9f674768271545200
waxkb <maxwellr2028@gmail.com>
2026-08-15 21:02:20 -0700

    e

--- nvim/.config/nvim/colors/base46-matugen.lua
+++ nvim/.config/nvim/colors/base46-matugen.lua
@@ -54,8 +54,8 @@ end
 
 if not base46.theme_tables[theme_name] or base46.theme_tables[theme_name].type ~= vim.o.background then
   local builtin = vim.deepcopy(assert(base46.get_builtin_theme(theme_base)))
-  local harmonized = base46.theme_harmonize(builtin, "#cd631b", harmony)
-  harmonized = base46.theme_set_bg(harmonized, "#1a120d")
+  local harmonized = base46.theme_harmonize(builtin, "#e6c8a4", harmony)
+  harmonized = base46.theme_set_bg(harmonized, "#231f1a")
 
   base46.theme_tables[theme_name] = harmonized
 end
--- scripts/tofiWall.sh
+++ scripts/tofiWall.sh
@@ -4,7 +4,7 @@ TOFI_BIN="tofi"
 selection=$($TOFI_BIN --config ~/.config/tofi/config < <(find "$WALLPAPER_DIR" -type f))
 
 if [[ -f "$selection" ]]; then
-  matugen image "$selection" --source-color-index 0 &
+  # matugen image "$selection" --source-color-index 0 &
   noctalia msg wallpaper-set "$selection" &
   dms ipc call wallpaper set "$selection" &
 
