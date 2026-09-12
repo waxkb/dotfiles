6cdf600f79e1c2d6bf417fd489b0c336433c49a7
waxkb <maxwellr2028@gmail.com>
2026-09-12 14:00:41 -0700

    e

---
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/todo-comments.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      highlight = {
+        pattern = [[.*<(KEYWORDS)\s*]],
+      },
+      search = {
+        pattern = [[\b(KEYWORDS)\b]],
+      },
+    },
+  },
+}
